622c6dce22205659a8eb0df7139cc2b16b603ba6
Fabio <s276842@studenti.polito.it>
2022-05-10 17:13:13 +0200

    new dataset + new models + utils

--- models.py
+++ models.py
@@ -1,180 +1,69 @@
 from transformers import AutoModel, AutoTokenizer
-from torch.nn import Module
+from torch import nn
 import torch
 
-class CLSExtractor:
-    def __call__(self, out):
-        return out.last_hidden_state[:, 0]
+#Mean Pooling - Take attention mask into account for correct averaging
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    return sum_embeddings / sum_mask
 
-class PoolerExtractor:
-    def __call__(self, out):
-        return out['pooler_output']
-
-
-class BaseTransformerEncoder(Module):
-    def __init__(self, model_name_or_path, outputlayer='cls', device='cpu'):
-        super(BaseTransformerEncoder, self).__init__()
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-        self.model = AutoModel.from_pretrained(model_name_or_path).to(device)
+class SentenceEncoder(nn.Module):
+    def __init__(self, model_name, device, truncation_side='right'):
+        super(SentenceEncoder, self).__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, truncation_side=truncation_side, device=device)
+        self.model = AutoModel.from_pretrained(model_name).to(device)
+        self.hidden_size = self.model.config.hidden_size
         self.device = device
 
-        if outputlayer == 'cls':
-            self.outputlayer = CLSExtractor()
-        elif outputlayer == 'pooler':
-            self.outputlayer = PoolerExtractor()
-        else:
-            raise ValueError
+    def forward(self, input):
+        encoded_input = self.tokenizer(input, padding=True, truncation=True, return_tensors='pt')
+        encoded_input = {key:val.to(self.device) for key, val in encoded_input.items()}
+        model_output = self.model(**encoded_input)
+        return mean_pooling(model_output, encoded_input['attention_mask'])
 
-    def forward(self, data):
-        data = self.tokenizer(data, padding=True, truncation=True, return_tensors='pt')
-        data = {key:val.to(self.device) for key, val in data.items()}
-        data = self.model(**data)
-        return self.outputlayer(data)
+class LinearHead(nn.Module):
+  def __init__(self, in_dim, out_dim, device, dropout = 0.3):
+    super().__init__()
+    self.linear = nn.Linear(in_features=in_dim, out_features=out_dim)
+    torch.nn.init.eye_(self.linear.weight)
+    # torch.nn.init.xavier_uniform_(self.classification.weight, gain=1.0)
+    # torch.nn.init.kaiming_normal_(self.classification.weight)
+    # torch.nn.init.eye_(self.classification.weight)
+    torch.nn.init.zeros_(self.linear.bias)
+    self.device = device
+    self.dropout = nn.Dropout(dropout)
+    self.to(device)
 
+  def forward(self, documents_embeddings):
+    docs = self.dropout(documents_embeddings)
+    scores = self.linear(docs)
+    return scores
 
-class RecurrentTransformerEncoder(BaseTransformerEncoder):
-    def __init__(self, model_name_or_path, outputlayer='cls', device='cpu'):
-        super(RecurrentTransformerEncoder, self).__init__(model_name_or_path, outputlayer, device)
-        # self.recurrent_model =
-        # self.first_hidden =
+class HeadedEncoder(nn.Module):
+    def __init__(self, base_encoder_name_or_path, device,
+                 heads=['domain', 'name', 'doc'], truncation_side='right'):
 
-    def forward(self, data):
-        data = super().forward(data)
+        super(HeadedEncoder, self).__init__()
+        self.sentence_encoder = SentenceEncoder(base_encoder_name_or_path, device=device, truncation_side=truncation_side)
+        self.hidden_size = self.sentence_encoder.hidden_size
 
-        return self.outputlayer(data)
+        if isinstance(heads, dict):
+            self.heads = heads
+        elif isinstance(heads, list):
+            self.heads = {key:LinearHead(self.hidden_size, self.hidden_size, device=device) for key in heads}
 
+    def forward(self, input, output=None):
+        input_encoding = self.sentence_encoder(input)
 
+        if output is not None:
+            if not hasattr(self, 'heads'):
+                raise AttributeError('The encoder has no heads ({})'.format(output))
+            elif isinstance(output, list):
+                return {key:self.heads[key](input_encoding) for key in output}
+            else:
+                return self.heads[output](input_encoding)
 
-class DenseRetriever:
-    def __init__(self, vectorized_knowledge_file, score_function, projections=['K'], topk=5):
-
-        tmp = torch.load(vectorized_knowledge_file)
-        self.projections = projections
-        self.topk = topk
-        # self.matrices = [tmp[p]['matrices'] for p in self.projections]
-        # self.indices = [tmp[p]['keys'] for p in self.projections]
-        self.score_function = score_function
-        self.projection_tree = {}
-
-        indices = [set()]
-        for p in projections:
-            p_matrix = tmp[p]['matrix']
-            p_indices = tmp[p]['keys']
-            start = 0
-            end = start
-            for ind in indices:
-
-                while end < len(p_indices) and len(set(ind).difference(p_indices[end])) == 0:
-                    end += 1
-
-                p_sliced_matrix = p_matrix[start:end, :]
-                p_sliced_indices = [tuple(k) for k in p_indices[start:end]]
-
-                self.projection_tree[tuple(ind)] = (p_sliced_matrix, p_sliced_indices)
-                start = end
-            indices = p_indices
-
-    def __call__(self, vector):
-        key = tuple()
-        p_matrix, p_indices = self.projection_tree[key]
-
-        for p in self.projections:
-            scores = self.score_function(vector, p_matrix)
-
-            if 'K' not in p:
-                max_ind = scores.argmax().item()
-                key = p_indices[max_ind]
-                p_matrix, p_indices = self.projection_tree[key]
-            else: # final prediction
-                topk_ids = scores.topk(self.topk).indices
-                key = [p_indices[ii.item()] for ii in topk_ids]
-
-        return key
-
-
-
-# class Encoder():
-#     def __init__(self, model, tokenizer, preprocessing_transform=None, postprocessing_transform=None):
-#         self.model = model
-#         self.tokenizer = tokenizer
-#         self.preprocessing_transform = preprocessing_transform
-#         self.postprocessing_transform = postprocessing_transform
-#
-#     #todo implement batches
-#     def __call__(self, item):
-#
-#         try:
-#             iter(item)
-#             data = item
-#         except:
-#             data = [item]
-#
-#         if self.preprocessing_transform is not None:
-#         #     x = [self.preprocessing_transform(val) for val in item]
-#             data = self.preprocessing_transform(data)
-#
-#
-#         tokenized_data = self.tokenizer(data, return_tensors='pt', padding=True, truncation=True)
-#         tokenized_data = {key:value.to(self.model.device) for key, value in tokenized_data.items()}
-#         out = self.model(**tokenized_data)
-#         del tokenized_data, data
-#
-#         if self.postprocessing_transform is not None:
-#             out = self.postprocessing_transform(out)
-#
-#         return out
-
-class Cosine:
-    def __call__(self, vector, matrix):
-        vector = vector.view(1,-1)
-        v_shape = vector.shape
-        m_shape = vector.shape
-
-        if v_shape[1] == m_shape[1]:
-            r = torch.cosine_similarity(vector, matrix)
-        else:
-            r = torch.cosine_similarity(vector, matrix.T)
-
-        return r.flatten()
-
-class DotProd:
-    def __call__(self, vector, matrix):
-        vector = vector.view(1,-1)
-        v_shape = vector.shape
-        m_shape = vector.shape
-
-        if v_shape[1] == m_shape[0]:
-            r = vector @ matrix
-        else:
-            r = vector @ matrix.T
-
-        return r.flatten()
-
-class Euclidean:
-
-    def __call__(self, vector, matrix):
-        vector = vector.view(1, -1)
-        v_shape = vector.shape
-        m_shape = vector.shape
-
-        if v_shape[1] == m_shape[1]:
-            r = torch.cdist(vector, matrix)
-        else:
-            r = torch.cdist(vector, matrix.T)
-
-        return r.flatten()
-
-
-if __name__ == '__main__':
-    # e = BaseTransformerEncoder('distilroberta-base')
-    # from dataset import KnowledgeBase
-    # k_base = KnowledgeBase('./DSTC9/data/knowledge.json')
-    # from transforms import ConcatenateKnowledge
-    #
-    # x = ConcatenateKnowledge()(k_base[0])
-    # e([x])
-
-    d = DenseRetriever('DSTC9/data_eval/vectorized_knowledge/all-mpnet-base-v2_cls.pt', torch.cosine_similarity, ['DE', 'K'])
-    for i in range(10):
-        x = d(torch.rand(1, 768), Euclidean())
-        print(x)
+        return input_encoding
---
+++ utils.py
@@ -0,0 +1,239 @@
+import sys
+
+from dataset import KnowledgeBase
+import itertools
+import json
+import os
+
+def flatmap(func, *iterable):
+    return list(itertools.chain.from_iterable(map(func, *iterable)))
+
+
+class ConcatenateDialogContext:
+    def __init__(self, user_special_token='', agent_special_token = '', reverse=False, limit=0):
+        self.speaker_special_tokens = {'U': user_special_token, 'S':agent_special_token}
+        self.reverse = reverse
+        self.limit = limit
+        self.f = lambda utterance: (self.speaker_special_tokens[utterance['speaker']], utterance['text'])
+
+    def __call__(self, dialog_context):
+        turns = dialog_context[-self.limit:]
+        if self.reverse is True:
+            turns = turns[::-1]
+
+        r = flatmap(self.f, turns)
+        return ' '.join(r)
+
+
+
+SPECIAL_TOKENS = {'domain':'<D>', 'name': '<E>', 'question': '<Q>', 'answer': '<A>', 'U': '<U>', 'S': '<S>'}
+NO_SPECIAL_TOKENS = {'domain':'', 'name': '', 'question': '', 'answer': '', 'U': '', 'S': ''}
+
+class ConcatenateKnowledge:
+    def __init__(self, keys=['domain', 'name', 'question', 'answer'], sep_tokens=NO_SPECIAL_TOKENS, replace_domain_name=False):
+        self.keys = keys
+        self.sep_tokens = sep_tokens
+        self.replace_domain_name = replace_domain_name
+        # if both domain and entity_name are required, we need to do an additional check
+        # for those domains that does not have entities, such as train and taxi
+        if 'name' in self.keys:
+            self.check_entity_name = True
+            self.alt_keys = ['domain-entity'] + [key for key in keys if key != 'domain' and key!= 'name']
+            self.sep_tokens['domain-entity'] = self.sep_tokens['name']
+        else:
+            self.check_entity_name = False
+
+    def __call__(self, knowledge_snippet):
+
+        # if the current knowledge snippet belongs to a domain without entities
+        if self.check_entity_name and knowledge_snippet.get('name') == None:
+            keys = self.alt_keys
+            knowledge_snippet['domain-entity'] = knowledge_snippet['domain']
+        else:
+            keys = self.keys
+            if self.replace_domain_name:
+                knowledge_snippet['name'] = knowledge_snippet['name'].replace(knowledge_snippet['domain'], '')
+
+        pairs_sep_key = [f'{self.sep_tokens[key]} {knowledge_snippet[key]} ' if knowledge_snippet.get(key) is not None else '' for key in keys]
+        return ''.join(pairs_sep_key).strip()
+
+
+# ==============================================================
+
+
+def check_domain(true, pred):
+    return true['domain'] == pred['domain']
+
+def check_entity(true, pred):
+    return str(true['entity_id']) == str(pred['entity_id']) and check_domain(true, pred)
+
+def check_document(true, pred):
+    return int(true['doc_id']) == int(pred['doc_id']) and check_entity(true, pred)
+
+def check_top_knowledge_snippets(true_label, retrieved_snippets):
+    topk = len(retrieved_snippets)
+    for i, k in enumerate(retrieved_snippets):
+        if check_document(true_label, k):
+            return True, (topk - i) % (topk + 1)
+
+    return (False, 0)
+
+
+def store_results(predictions, output_path):
+    '''The labels for entity_id and doc_id are of type <str> in the knowledge base and of type <int> in the labels'''
+
+    r = []
+    for label in predictions:
+        if label['target'] == False:
+            r.append(label)
+        else:
+            best_ks = []
+            for k in label['knowledge']:
+                ks = {'domain' : k['domain']}
+
+                ks['entity_id'] = int(entity_id) if (entity_id := k.get('entity_id')) != None and entity_id != '*' else '*'
+                if (doc_id := k.get('doc_id')) != None:
+                    ks['doc_id'] = int(doc_id)
+
+                best_ks.append(ks)
+
+            label['knowledge'] = best_ks
+            r.append(label)
+
+    with open(output_path, 'w') as f:
+        json.dump(predictions, f)
+
+    return
+
+
+def score_results(dataroot, dataset, out_file, score_file):
+    os.system(
+        f'python scripts/scores.py --dataset "{dataset}" --dataroot "{dataroot}" --outfile "{out_file}" --scorefile "{score_file}"')
+    with open(score_file, 'r') as f:
+        scores = json.load(f)
+        mrr = scores['selection']['mrr@5']
+        r1 = scores['selection']['r@1']
+        r5 = scores['selection']['r@5']
+
+    return {'r@1':r1, 'r@5':r5, 'mrr':mrr}
+
+
+
+# ====================================================================
+
+
+def get_domain_corpus(knowledge_base: KnowledgeBase, knowledge_preprocessing):
+    corpus_ids = [{'domain':domain} for domain in knowledge_base.keys()]
+    corpus = [knowledge_preprocessing(ks) for ks in corpus_ids]
+
+    return corpus, corpus_ids
+
+def get_hierarchical_domain_corpus(knowledge_base: KnowledgeBase):
+    corpus_ids = {'': [{'domain':domain} for domain in knowledge_base.keys()]}
+    corpus = corpus_ids
+
+    return corpus, corpus_ids
+
+def get_hierarchical_entity_corpus(knowledge_base: KnowledgeBase):
+    corpus = {}
+    corpus_ids = {}
+    for domain in knowledge_base.keys():
+        corpus[domain] = []
+        corpus_ids[domain] = []
+        for entity_id in knowledge_base[domain]:
+            name = knowledge_base[domain][entity_id]['name']
+            corpus_ids[domain].append({'domain':domain, 'entity_id':entity_id, 'name':name})
+            corpus[domain].append(name)
+
+    return corpus, corpus_ids
+
+
+def get_entity_corpus(knowledge_base: KnowledgeBase, knowledge_preprocessing):
+    corpus_ids = []
+    for domain in knowledge_base.keys():
+        domain_dict = knowledge_base[domain]
+        for entity_id in domain_dict:
+            entity_dict = domain_dict[entity_id]
+            d = {'domain': domain, 'entity_id':entity_id}
+            for key, val in entity_dict.items():
+                if key != 'docs':
+                    d[key] = val
+            corpus_ids.append(d)
+
+    corpus = [knowledge_preprocessing(ks) for ks in corpus_ids]
+    return corpus, corpus_ids
+
+
+def get_hierarchical_document_corpus(knowledge_base: KnowledgeBase):
+    corpus = {}
+    corpus_ids = {}
+    for domain in knowledge_base.keys():
+        for entity_id in knowledge_base[domain]:
+            docs_dict = knowledge_base[domain][entity_id]['docs']
+            corpus_ids[(domain, entity_id)] = [{'domain':domain, 'entity_id':entity_id, 'doc_id':doc_id} for doc_id in docs_dict.keys()]
+            corpus[(domain, entity_id)] = [' '.join(docs_dict[doc_id].values()) for doc_id in docs_dict.keys()]
+
+    return corpus, corpus_ids
+
+
+def get_document_corpus(knowledge_base, knowledge_preprocessing=None):
+    if knowledge_preprocessing is None:
+        corpus = [doc for doc in knowledge_base]
+    else:
+        corpus = [knowledge_preprocessing(doc) for doc in knowledge_base]
+
+    corpus_ids = [knowledge_base.get_keys(i) for i in range(len(knowledge_base))]
+    return corpus, corpus_ids
+
+
+
+#========================= Print ======================
+
+def print_example(method, query, candidates, scores, knowledge_base, label=None, fp=sys.stdout, knowledge_preprocessing=None):
+
+    if method == 'document':
+        doc_candidates = [knowledge_base.get_doc(**candidate) for candidate in candidates]
+        doc_label = knowledge_base.get_doc(**label) if label is not None else None
+    else:
+        doc_candidates = candidates
+        doc_label = label
+
+    if knowledge_preprocessing is not None:
+        doc_candidates = [knowledge_preprocessing(doc) for doc in doc_candidates]
+        if doc_label is not None:
+            doc_label = knowledge_preprocessing(doc_label)
+
+
+
+    print('_'*40, 'Example', '_'*40, file=fp)
+    print("Query:", file=fp)
+    for i in range(0, len(query), 80):
+        print('\t', query[i:i+80], file=fp)
+
+
+    print(f"\n{method} predictions:", file=fp)
+
+    for candidate, score in zip(doc_candidates, scores):
+        print("(Score: {:.4f})".format(score), candidate, file=fp)
+
+    if label is not None:
+        print(f'TRUE: {doc_label}', file=fp)
+    print('_' *89, file=fp)
+
+
+import random
+def print_title(title, sep='='):
+    print('\n', sep*20, title, sep*20)
+
+def print_triplet(anchor, pos, neg):
+    print(f'{"anchor":<7}:', anchor[:100] + ('...' if len(anchor)>=100 else ''))
+    print(f'{"pos":<7}:', pos[:100]+ ('...' if len(pos)>=100 else ''))
+    print(f'{"neg":<7}:', neg[:100]+ ('...' if len(neg)>=100 else ''))
+    print()
+
+if __name__ == '__main__':
+    k_pre = ConcatenateKnowledge()
+    kb = KnowledgeBase('./DSTC9/data')
+
+    x,y = get_document_corpus(kb, k_pre)
+
